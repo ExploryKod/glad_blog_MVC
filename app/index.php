6a4c8a564749163a0a37541ee02125d4623cf0d5
besrour.mohamed <besrour.mohamed@llw.local>
2022-11-17 16:52:21 +0100

    a2enmod rewrite

--- app/index.php
+++ app/index.php
@@ -1,7 +1,11 @@
 <?php
 
 use Gladblog\Controller\PostController;
+use Symfony\Component\Yaml\Yaml;
 
 require_once "vendor/autoload.php";
+
+$yaml = Yaml::parseFile(dirname(__FILE__) . "/config/routes.yml");
+var_dump($yaml); die;
 
 $controller = new PostController();
